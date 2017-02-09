57e6c54b4e13fabeccde0d62ae052416cdffeb92
Shankar <shankar21mail@gmail.com>
2017-02-09 12:24:20 +0530

    fix/protocol_handling: resolve protocol handling
    Resolved protocol handling.

--- src/protocol.js
+++ src/protocol.js
@@ -5,19 +5,38 @@ const protocol = require('electron').protocol;
 
 const safeScheme = 'safe';
 
-const authoriseApp = () => {
-  return safeApp.initializeApp(appInfo)
-    .then(app => app.auth.connectUnregistered());
+const appInfo = {
+  'id': 'net.maidsafe.examples.webclient',
+  'name': 'Safe app - web client',
+  'vendor': 'MaidSafe.net Ltd.'
 };
 
-const appObj = authoriseApp();
+let appObj = null;
+
+const authoriseApp = () => {
+  if (appObj) {
+    return Promise.resolve(true);
+  }
+  return safeApp.initializeApp(appInfo)
+    .then(app => app.auth.connectUnregistered())
+    .then((app) => (appObj = app));
+};
+
+const fetchData = (url) => {
+  if (!appObj) {
+    return Promise.reject(new Error('Unable to create unregistered client'));
+  }
+  return appObj.webFetch(url)
+    .then((f) => appObj.immutableData.fetch(f.dataMapName))
+    .then((i) => i.read())
+};
 
 const registerSafeAuthProtocol = () => {
-  protocol.registerBufferProtocol(safeAuthScheme, (req, cb) => {
-    appObj.webFetch(req.url)
-      .then((f) => app.immutableData.fetch(f.dataMapName))
-      .then((i) => i.read())
-      .then((co) => cb({ mimeType: 'text/html', data: co }));
+  protocol.registerBufferProtocol(safeScheme, (req, cb) => {
+    authoriseApp()
+      .then(() => fetchData(req.url))
+      .then((co) => cb({ mimeType: 'text/html', data: co }))
+      .catch(console.error);
   }, (err) => {
     if (err) console.error('Failed to register protocol');
   });
