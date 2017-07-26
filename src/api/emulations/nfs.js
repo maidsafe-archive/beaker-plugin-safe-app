113844af2f3fa92428070e5ffc425d5951d0a517
hunterlester <guilfordhunter@gmail.com>
2017-07-26 16:30:32 -0700

    addition of File methods

--- src/api/emulations/nfs.js
+++ src/api/emulations/nfs.js
@@ -1,5 +1,12 @@
 const { genHandle, getObj, freeObj } = require('../helpers');
 
+// File-open constants
+const OPEN_MODE_OVERWRITE = 1;
+const OPEN_MODE_APPEND = 2;
+const OPEN_MODE_READ = 4;
+const FILE_READ_FROM_BEGIN = 0;
+const FILE_READ_TO_END = 0;
+
 module.exports.manifest = {
   create: 'promise',
   fetch: 'promise',
@@ -7,18 +14,22 @@ module.exports.manifest = {
   update: 'promise',
   getFileMeta: 'promise',
   free: 'sync',
-  freeFile: 'sync'
+  freeFile: 'sync',
+  open: 'promise',
+  size: 'promise',
+  read: 'promise',
+  write: 'promise',
+  close: 'promise'
 };
 
 /**
- * Create a new file with the given content, put the content
- * on the network via ImmutableData (public) and wrap it into a File.
+ * Helper function to create, write to file, and commit to network
  * @name window.safeNfs.create
  *
  * @param {NFSHandle} nfsHandle the NFS emulation handle
  * @param {(String|Buffer)} content
  *
- * @returns {Promise<FileHandle>} the File handle of a newly created file
+ * @returns {Promise<File>}
  **/
 module.exports.create = (nfsHandle, content) => {
   return getObj(nfsHandle)
@@ -33,7 +44,7 @@ module.exports.create = (nfsHandle, content) => {
  * @param {NFSHandle} nfsHandle the NFS emulation handle
  * @param {String} fileName - the path/file name
  *
- * @returns {Promise<FileHandle>} the handle of the File found for that path
+ * @returns {Promise<FileContextHandle>} the handle of the File found for that path
  **/
 module.exports.fetch = (nfsHandle, fileName) => {
   return getObj(nfsHandle)
@@ -50,7 +61,7 @@ module.exports.fetch = (nfsHandle, fileName) => {
  * @param {FileHandle} fileHandle the handle of the File to serialise and store
  * @param {(String|Buffer)} fileName the path to store the file under
  *
- * @returns {Promise<FileHandle>} the same File handle
+ * @returns {Promise<FileContextHandle>} the same File handle
  **/
 module.exports.insert = (nfsHandle, fileHandle, fileName) => {
   return getObj(nfsHandle)
@@ -69,7 +80,7 @@ module.exports.insert = (nfsHandle, fileHandle, fileName) => {
  * @param {(String|Buffer)} fileName - the path to store the file under
  * @param {Number} version the version successor, to ensure you
          are overwriting the right one
- * @returns {Promise<FileHandle>} the same File handle
+ * @returns {Promise<FileContextHandle>} the same File handle
  **/
 module.exports.update = (nfsHandle, fileHandle, fileName, version) => {
   return getObj(nfsHandle)
@@ -93,7 +104,7 @@ module.exports.update = (nfsHandle, fileHandle, fileName, version) => {
  * Retrieve the file's metadata.
  * @name window.safeNfs.getFileMeta
  *
- * @param {FileHandle} fileHandle the File handle
+ * @param {FileContextHandle} fileHandle the File handle
  *
  * @returns {FileMetadata} the file's metadata
  **/
@@ -124,6 +135,70 @@ module.exports.free = (nfsHandle) => freeObj(nfsHandle);
  * @param {FileHandle} fileHandle the File handle
  **/
 module.exports.freeFile = (fileHandle) => freeObj(fileHandle);
+
+// QUESTION: Separation of NFS and File class methods in safe_app_nodejs /
+// helps to make sense of each grouping of functions.
+// Are these additions confusing or clear?
+
+/**
+* Opens a given file, or creates a new file if none is given
+* Does not commit file to network
+* @name window.safeNfs.open
+*
+* @param {File} file
+* @param {Number} openMode
+* @returns {FileContextHandle} the file context handle of a newly created file
+**/
+module.exports.open = (file, openMode) => {
+  return obj.netObj.open(file, openMode)
+  .then((fileCtx) => genHandle(obj.app, fileCtx));
+};
+
+
+/**
+* Write to file that's opened in write or append mode
+* @name window.safeNfs.write
+*
+* @param {FileContextHandle} fileContextHandle
+* @param {String|Buffer} content
+*
+* @returns {Promise} the File handle of a newly created file
+**/
+module.exports.write = (fileContextHandle, content) => {
+  return getObj(fileContextHandle)
+  .then((obj) => obj.netObj.write(content));
+};
+
+
+
+/**
+* Close file, thereby committing it to the network
+* @name window.safeNfs.close
+*
+* @param {FileContextHandle} fileContextHandle the NFS emulation handle
+*
+* @returns {Promise}
+**/
+module.exports.close = (fileContextHandle) => {
+  return getObj(fileContextHandle)
+  // QUESTION: Should we also free handle here? Or leave it to be freed separately?
+  //freeObj(fileContextHandle) ?
+  .then((obj) => obj.netObj.close());
+};
+
+/**
+* Get the byte size of a file
+* @name window.safeNfs.size
+*
+* @param {FileContextHandle} fileContextHandle
+*
+* @returns {Number} the File handle of a newly created file
+**/
+module.exports.size = (fileContextHandle) => {
+  return getObj(fileContextHandle)
+  .then((obj) => obj.netObj.size())
+  .then((int) => int);
+};
 
 /**
  * @name NFSHandle
